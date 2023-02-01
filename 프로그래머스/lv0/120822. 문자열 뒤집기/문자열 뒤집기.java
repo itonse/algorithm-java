e8abd46079b38fe5111877e468beca6cfa397f53
itonse <76129297+itonse@users.noreply.github.com>
2023-02-01 10:34:35 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.06 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public StringBuffer solution(String my_string) {
+        StringBuffer answer = new StringBuffer(my_string);
+        answer = answer.reverse();
+        
+        return answer;
+    }
+}
