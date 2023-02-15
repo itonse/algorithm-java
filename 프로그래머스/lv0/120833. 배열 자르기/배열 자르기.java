c392a46d044c36f2ac0024f6013c500d25b424b7
홍지은 <76129297+itonse@users.noreply.github.com>
2023-02-15 16:49:47 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -1,11 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int[num2 - num1 + 1];
-        int cnt = 0;
         
-        for(int i = num1; i <= num2; i++) {
-            answer[cnt++] = numbers[i];
-        }
+        int[] answer = Arrays.copyOfRange(numbers, num1, num2+1);
         
         return answer;
     }
