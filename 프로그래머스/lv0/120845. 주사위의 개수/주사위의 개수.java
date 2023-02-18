1b4f0d4da953e85263778947937dbc35132c9035
홍지은 <76129297+itonse@users.noreply.github.com>
2023-02-18 20:36:17 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] box, int n) {
+        int answer = 1;
+        
+        answer *= (box[0] / n);
+        answer *= (box[1] / n);
+        answer *= (box[2] / n);
+        
+        return answer;
+    }
+}
