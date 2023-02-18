132b4437d75b7b93170b8937c4d75095ad82db8f
홍지은 <76129297+itonse@users.noreply.github.com>
2023-02-18 21:15:52 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.47 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/README.md
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (1) - 120847 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120847) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120847?language=java) 
 
 ### 성능 요약
 
-메모리: 79.7 MB, 시간: 9.45 ms
+메모리: 75.3 MB, 시간: 0.47 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,29 +1,14 @@
+import java.util.Arrays;
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
         int max = -1;
         int subMax = -1;
-        int cnt = 0;
+
+        Arrays.sort(numbers);
         
-        for(int i = 0; i < numbers.length; i++) {
-            if(numbers[i] >= max) {
-                max = numbers[i];
-                System.out.println("max = " + max);;
-            }
-        }
-        
-        for(int j = 0; j < numbers.length; j++) {
-            if(numbers[j] == max) {
-                cnt++;
-                if(cnt == 2) {
-                    subMax = max;
-                    break;
-                }
-            }
-            else if(numbers[j] >= subMax) {
-                subMax = numbers[j];
-            }
-        }
+        max = numbers[numbers.length - 1];
+        subMax = numbers[numbers.length - 2];
         
         answer = max * subMax;
         return answer;
