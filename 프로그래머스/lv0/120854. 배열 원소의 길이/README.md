480d07bbe2d80598f36d9612bb3f5609979baba6
홍지은 <76129297+itonse@users.noreply.github.com>
2023-02-20 20:24:00 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/README.md
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 원소의 길이 - 120854 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120854) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120854?language=java) 
 
 ### 성능 요약
 
-메모리: 73.5 MB, 시간: 1.97 ms
+메모리: 77.4 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -1,19 +1,11 @@
-import java.util.ArrayList;
-
 class Solution {
     public int[] solution(String[] strlist) {
-        ArrayList list = new ArrayList<Integer>();
-        int num = 0;
+        int[] answer = new int[strlist.length];
 
         for(int i = 0; i < strlist.length; i++) {
-            num = strlist[i].length();
-            list.add(num);
+            answer[i] = strlist[i].length();
         }
 
-        int[] answer = new int[list.size()];
-
-        answer = list.stream().mapToInt(i -> (int) i).toArray();
-        
         return answer;
     }
 }
