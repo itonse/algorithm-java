feb770eb33ebde59d167c67c84d6331b24e61fed
홍지은 <76129297+itonse@users.noreply.github.com>
2023-02-27 00:36:41 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.16 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/README.md
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숨어있는 숫자의 덧셈 (2) - 120864 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120864) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120864?language=java) 
 
 ### 성능 요약
 
-메모리: 78.2 MB, 시간: 0.02 ms
+메모리: 76.6 MB, 시간: 0.16 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,19 +1,14 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        int num = 0;
+        String[] str = my_string.replaceAll("[A-z]", " ").split(" ");
 
-        for(int i = 0; i < my_string.length(); i++) {
-            if(my_string.charAt(i) - '0' <= 9) {
-                num *= 10;
-                num += my_string.charAt(i) - '0';
-            } else {
-                answer += num;
-                num = 0;
+        for(int i = 0; i < str.length; i++) {
+            if(!str[i].equals("")) {
+                answer += Integer.parseInt(str[i]);
             }
         }
         
-        answer += num;
         return answer;
     }
 }
