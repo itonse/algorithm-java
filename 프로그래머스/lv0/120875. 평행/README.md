95727899bd17221f90ae6670bb77dbdfd862cfc7
홍지은 <76129297+itonse@users.noreply.github.com>
2023-03-01 12:22:27 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/README.md
+++ 프로그래머스/lv0/120875. 평행/README.md
@@ -1,10 +1,10 @@
 # [level 0] 평행 - 120875 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875?language=java) 
 
 ### 성능 요약
 
-메모리: 73.8 MB, 시간: 0.02 ms
+메모리: 78.7 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -4,37 +4,29 @@ class Solution {
         float incline1;
         float incline2;
         boolean flag = false;
-
+        
+        loop:
         for(int i = 0; i < dots.length - 1; i++) {
             for(int j = i + 1; j < dots.length; j++) {
                 for(int k = 0; k < dots.length; k++) {
-                    if(k == i || k == j) {
+                    if (k == i || k == j) {
                         continue;
                     }
                     for(int r = 0; r < dots.length; r++) {
-                        if(r == i || r == j || r == k) {
+                        if (r == i || r == j || r == k) {
                             continue;
                         }
 
                         incline1 = (float) (dots[i][1] - dots[j][1]) / (dots[i][0] - dots[j][0]);
                         incline2 = (float) (dots[k][1] - dots[r][1]) / (dots[k][0] - dots[r][0]);
-                        
-                        if(incline1 == incline2) {
+
+                        if (incline1 == incline2) {
                             answer = 1;
                             flag = true;
-                            break;
+                            break loop;
                         }
                     }
-                    if(flag) {
-                        break;
-                    }
                 }
-                if(flag) {
-                    break;
-                }
-            }
-            if(flag) {
-                break;
             }
         }
         
