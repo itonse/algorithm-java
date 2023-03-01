15f58953fdc2a2becb050e4ee3089760c17a2bbd
홍지은 <76129297+itonse@users.noreply.github.com>
2023-03-01 11:03:14 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.09 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/README.md
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/README.md
@@ -1,10 +1,10 @@
 # [level 0] 저주의 숫자 3 - 120871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120871) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120871?language=java) 
 
 ### 성능 요약
 
-메모리: 71.7 MB, 시간: 0.03 ms
+메모리: 74.5 MB, 시간: 0.09 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -3,29 +3,11 @@ class Solution {
         int answer = 0;
         int cnt = 0;
         int num;
-        boolean flag;
 
         for(int i = 0; i < n; i++) {
-            flag = true;
             cnt++;
-
-            while(flag) {;
-                flag = false;
-
-                if (cnt % 3 == 0) {
-                    cnt++;
-                    flag = true;
-                } else {
-                    num = cnt;
-                    while (num > 0) {
-                        if (num % 10 == 3) {
-                            cnt++;
-                            flag = true;
-                            break;
-                        }
-                        num /= 10;
-                    }
-                }
+            if(cnt % 3 == 0 || String.valueOf(cnt).contains("3")) {
+                i--;
             }
         }
         
