3c3dccae09dac424873ee6c9c763fbf69ee1c70d
홍지은 <76129297+itonse@users.noreply.github.com>
2023-03-01 12:18:33 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int solution(int[][] dots) {
+        int answer = 0;
+        float incline1;
+        float incline2;
+        boolean flag = false;
+
+        for(int i = 0; i < dots.length - 1; i++) {
+            for(int j = i + 1; j < dots.length; j++) {
+                for(int k = 0; k < dots.length; k++) {
+                    if(k == i || k == j) {
+                        continue;
+                    }
+                    for(int r = 0; r < dots.length; r++) {
+                        if(r == i || r == j || r == k) {
+                            continue;
+                        }
+
+                        incline1 = (float) (dots[i][1] - dots[j][1]) / (dots[i][0] - dots[j][0]);
+                        incline2 = (float) (dots[k][1] - dots[r][1]) / (dots[k][0] - dots[r][0]);
+                        
+                        if(incline1 == incline2) {
+                            answer = 1;
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if(flag) {
+                        break;
+                    }
+                }
+                if(flag) {
+                    break;
+                }
+            }
+            if(flag) {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
