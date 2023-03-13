fc2c72e238c4a396b2fa492da0a0ac3b51a093e3
홍지은 <76129297+itonse@users.noreply.github.com>
2023-03-14 00:08:49 +0900

    [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -2,12 +2,10 @@ class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
 
-
         while(n >= a) {
-            int num = n;
-            int plus = num / a * b;
+            int plus = n / a * b;
             answer += plus;
-            n = plus + num % a;
+            n = plus + n % a;
         }
         
         return answer;
