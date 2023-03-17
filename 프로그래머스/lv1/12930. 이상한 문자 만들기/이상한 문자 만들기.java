2e9ac28683507b64e1d967c4eb899adaec559eea
홍지은 <76129297+itonse@users.noreply.github.com>
2023-03-17 16:16:51 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.47 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,35 +1,12 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        boolean isBig;
+        String[] arr = s.split("");
         int cnt = 0;
-
-
-        for (int i = 0; i < s.length(); i++) {
-            isBig = true;
-            if (s.charAt(i) - 'a' >= 0) {
-                isBig = false;
-            }
-
-            if (s.charAt(i) == ' ') {
-                answer += ' ';
-                cnt = 0;
-                continue;
-            } else if(cnt % 2 == 0) {
-                if (isBig) {
-                    answer += s.charAt(i);
-                } else {
-                    answer += (char) (s.charAt(i) - 'a' + 'A');
-                }
-            } else {
-                if (isBig) {
-                    answer += (char) (s.charAt(i) - 'A' + 'a');
-                } else {
-                    answer += s.charAt(i);
-                }
-            }
-
-            cnt++;
+        
+        for (String ss: arr) {
+            cnt = ss.equals(" ") ? 0 : cnt + 1;
+            answer += cnt % 2 == 1 ? ss.toUpperCase() : ss.toLowerCase();
         }
         
         return answer;
