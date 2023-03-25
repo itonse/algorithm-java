26487d1bf01d9c9164027c9c41659cb57e8f0e1e
홍지은 <76129297+itonse@users.noreply.github.com>
2023-03-25 19:06:30 +0900

    [level 2] Title: H-Index, Time: 0.47 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public static int solution(int[] citations) {
+        Arrays.sort(citations);
+        int len = citations.length;
+
+        int max = 0;
+        for (int i = 1; i <= citations.length; i++) {  
+            if (i <= citations[len - i]) {  
+                max = i;
+            } else {
+                break;
+            }
+        }
+        
+        return max;
+    }
+}
