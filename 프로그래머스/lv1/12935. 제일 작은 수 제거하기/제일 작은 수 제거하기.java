fe7ed0ef7d5b8e7806d60ea09eb99a1673bd9684
홍지은 <76129297+itonse@users.noreply.github.com>
2023-04-01 00:08:02 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.08 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,32 +1,28 @@
 class Solution {
     public int[] solution(int[] arr) {
-        int min = arr[0];
-      int[] answer = new int[arr.length - 1];
+        int[] answer = new int[arr.length - 1];
 
-      if (arr.length <= 1) {
-         int[] answer2 = { -1 };
-         return answer2;
-      }
+        if (arr.length == 1) {
+            answer = new int[1];
+            answer[0] = -1;
+            return answer;
+        }
 
-
-         for (int i = 0; i < arr.length; i++) {
-
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
             if (min > arr[i]) {
-               min = arr[i];
+                min = arr[i];
             }
+        }
 
-         }
-           int index = 0;
-         for (int j = 0; j < arr.length; j++) {
-            if (min == arr[j]) {
-               continue;
-            } else { 
-               answer[index++] = arr[j];
+        int idx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == min) {
+                continue;
             }
+            answer[idx++] = arr[i];
+        }
 
-         }
-
-
-      return answer;
+        return answer;
     }
 }
