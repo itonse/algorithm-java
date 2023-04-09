82aaf82b1de98b7d63bb94e9af7a36eb6739408b
홍지은 <76129297+itonse@users.noreply.github.com>
2023-04-09 23:33:47 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,25 +1,11 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] numbers) {
-        int answer = -1;
-
-        Arrays.sort(numbers);
-
-        int start = numbers[0];
-        int end = numbers[numbers.length - 1];
-        int sum = 0;
-        int sum2 = 0;
-
-        for (int i = 0; i <= 9; i++) {
-            sum += i;
-        }
+        int answer = 45;
 
         for (int i: numbers) {
-            sum2 += i;
+            answer -= i;
         }
-
-        answer = sum - sum2;
+        
         return answer;
     }
 }
