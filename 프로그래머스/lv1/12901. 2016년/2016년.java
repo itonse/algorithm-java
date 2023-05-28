a27a8ee6c94ea52d02d921b0ed7bc6fa291e651e
홍지은 <76129297+itonse@users.noreply.github.com>
2023-05-28 23:28:52 +0900

    [level 1] Title: 2016년, Time: 1.32 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -3,8 +3,6 @@ import java.time.LocalDateTime;
 
 class Solution {
     public String solution(int a, int b) {
-        DayOfWeek dayOfWeek = LocalDateTime.of(2016, a, b, 0, 0).getDayOfWeek();
-        String dow = dayOfWeek.name();
-        return dow.substring(0, 3);
+        return LocalDateTime.of(2016, a, b, 0, 0).getDayOfWeek().toString().substring(0, 3);
     }
 }
--- 프로그래머스/lv1/12901. 2016년/README.md
+++ 프로그래머스/lv1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=java) 
 
 ### 성능 요약
 
-메모리: 74.4 MB, 시간: 1.20 ms
+메모리: 70.3 MB, 시간: 1.32 ms
 
 ### 구분
 
