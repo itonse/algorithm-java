124f92842b7a90606ded5349764149ef82b77755
itonse <govl3dnjs@gmail.com>
2023-07-20 23:51:25 +0900

    [level 3] Title: 최고의 집합, Time: 0.04 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
@@ -5,28 +5,18 @@ class Solution {
         int[] answer = new int[n];
         int num = s / n;
         int remain = s % n;
-        
+
         if (n > s) {
             int[] answer2 = {-1};
             return answer2;
         }
-        
-        if (num == 0) {
-            int idx = n;
-            while (remain > 0) {
-                ++answer[--idx];
-                remain--;
-            }
-        } else {
-            Arrays.fill(answer, num);
 
-            if (remain != 0) {
-                int idx = n;
-                while (remain > 0) {
-                    ++answer[--idx];
-                    remain--;
-                }
-            }
+        Arrays.fill(answer, num);
+        
+        int idx = n;
+        while (remain > 0) {
+            ++answer[--idx];
+            remain--;
         }
 
         return answer;
