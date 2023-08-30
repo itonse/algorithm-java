0bbd0760d22c12c68b531b23bfb0c5e9f49eb1be
itonse <govl3dnjs@gmail.com>
2023-08-30 21:40:51 +0900

    [level 2] Title: 주식가격, Time: 9.51 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,20 +1,17 @@
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-
-        for (int i = 0; i < prices.length; i++) {
-            int cnt = 0;
-            while (i + cnt + 1 < prices.length) {
-                if (prices[i] <= prices[i + cnt + 1]) {
-                    cnt++;
-                } else {
-                    cnt++;
+        int len = prices.length;
+        int[] answer = new int[len];
+        
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                answer[i]++;
+                if (prices[i] > prices[j]) {
                     break;
                 }
             }
-            answer[i] = cnt;
         }
-
+        
         return answer;
     }
 }
