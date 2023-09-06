40bc0c0d32348c6a517f960165f39cf63f613c9c
itonse <govl3dnjs@gmail.com>
2023-09-06 23:59:50 +0900

    [level 2] Title: N개의 최소공배수, Time: 17.38 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,17 +1,25 @@
 class Solution {
     public int solution(int[] arr) {
-        int cnt = 0;
-        int answer = 0;
-        while (cnt != arr.length) {
-            answer++;
-            cnt = 0;
-            for (int i = 0; i < arr.length; i++) {
-                if (answer % arr[i] == 0) {
-                    cnt++;
+   
+        java.util.Arrays.sort(arr);
+        
+        int answer = arr[arr.length - 1];
+
+        while (true) {
+            boolean isLCM = true;
+
+            for (int num : arr) {
+                if (answer % num != 0) {
+                    isLCM = false;
+                    break;
                 }
             }
+
+            if (isLCM) {
+                return answer;
+            }
+
+            answer++;
         }
-        
-        return answer;
     }
 }
