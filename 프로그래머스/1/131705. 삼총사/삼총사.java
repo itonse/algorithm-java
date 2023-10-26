dca0f3ddb8e74c186c256697ec92d96d9eb091dc
홍지은 <govl3dnjs@gmail.com>
2023-10-26 22:37:39 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -3,19 +3,11 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
-        int sum = 0;
-
-        Arrays.sort(number);   
-
+  
         for (int i = 0; i < number.length - 2; i++) {
             for (int j = i + 1; j < number.length - 1; j++) {
                 for (int k = j + 1; k < number.length; k++) {
-                    sum = number[i] + number[j] + number[k];
-
-                    if (sum > 0 && number[i] >= 0) {
-                        break;
-                    }
-                    if (sum == 0) {
+                    if (number[i] + number[j] + number[k] == 0) {
                         answer++;
                     }
                 }
