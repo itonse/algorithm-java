f1482a9dbaa9094fd6089e7d9d32ce070fe11988
홍지은 <govl3dnjs@gmail.com>
2023-10-31 23:09:49 +0900

    [level 1] Title: 추억 점수, Time: 0.04 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+
+        HashMap map = new HashMap();
+
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+
+        for (int i = 0; i < photo.length; i++) {
+            for (int j = 0; j < photo[i].length; j++) {
+                if (map.containsKey(photo[i][j])) {
+                    answer[i] += (int) map.get(photo[i][j]);
+                }
+            }
+        }
+
+        return answer;
+    }
+}
