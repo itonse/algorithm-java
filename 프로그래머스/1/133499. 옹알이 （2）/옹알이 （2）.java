6e039cf7fcafe4e1aa0b9b011b61391cddacef75
Jieun Hong <govl3dnjs@gmail.com>
2023-11-14 02:17:22 +0900

    [level 1] Title: 옹알이 (2), Time: 0.59 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -3,23 +3,17 @@ class Solution {
         int answer = 0;
 
         for (String str : babbling) {
-            str = str.replace("aya", "1");
-            str = str.replace("ye", "2");
-            str = str.replace("ma", "3");
-            str = str.replace("woo", "4");
-
-            boolean isAnswer = true;
-            for (int idx = 0; idx < str.length(); idx++) {
-                if (Character.isLowerCase(str.charAt(idx))) {
-                    isAnswer = false;
-                    break;
-                }
-                if (idx < str.length() - 1 && str.charAt(idx) == str.charAt(idx + 1)) {
-                    isAnswer = false;
-                }
+            if (str.contains("ayaaya") || str.contains("yeye") || str.contains("mama") || str.contains("woowoo")) {
+                continue;
             }
             
-            if (isAnswer) answer++;
+            str = str.replace("aya", " ");
+            str = str.replace("ye", " ");
+            str = str.replace("ma", " ");
+            str = str.replace("woo", " ");
+            str = str.replace(" ", "");
+            
+            if (str.length() == 0) answer++;
         }
 
         return answer;
