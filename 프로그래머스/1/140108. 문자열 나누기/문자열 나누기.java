65fc625e743d7a33a373e87ecd0aaf53362bbd05
Jieun Hong <govl3dnjs@gmail.com>
2023-11-19 15:19:55 +0900

    [level 1] Title: 문자열 나누기, Time: 0.47 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,25 +1,22 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        char target = s.charAt(0);
+        char target = 0;
         int sameCnt = 0;
-        int otherCnt = 0;
 
-        for (int idx = 0; idx < s.length(); idx++) {
-            if (s.charAt(idx) == target) {
-                sameCnt++;
-            } else {
-                otherCnt++;
+        for (char ch : s.toCharArray()){
+            if (sameCnt == 0) {
+                target = ch;
             }
 
-            if (sameCnt == otherCnt) {
-                answer++;
-                sameCnt = 0;
-                otherCnt = 0;
+            if (ch == target) {
+                sameCnt++;
+            } else {
+                sameCnt--;
+            }
 
-                if (idx < s.length() - 1) {
-                    target = s.charAt(idx + 1);
-                }
+            if (sameCnt == 0) {
+                answer++;
             }
         }
 
