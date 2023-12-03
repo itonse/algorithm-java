a7cb80191236f8acd6c60716c78e94fbb3f5b8fc
Jieun Hong <govl3dnjs@gmail.com>
2023-12-03 16:03:12 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.12 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+
+        for (int num : ingredient) {
+            stack.push(num);
+            if (stack.size() >= 4 && stack.get(stack.size() - 1) == 1 && stack.get(stack.size() - 2) == 3 && stack.get(stack.size() - 3) == 2 && stack.get(stack.size() - 4) == 1) {
+                for (int i = 0; i < 4; i++) {
+                    stack.pop();
+                }
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
