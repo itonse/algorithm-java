d4ea4ad5a9e18d013aadefe02dd65ca9b298dea8
Jieun Hong <govl3dnjs@gmail.com>
2023-12-18 04:37:07 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 74.8 MB, 시간: 0.06 ms
+메모리: 72.5 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 0일 23:59:50
+2023년 12월 1일 4:37:5
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,35 +1,31 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
 
-        Map<String, Integer> target = new HashMap<>();
+        HashMap<String, Integer> wantMap = new HashMap<>();
+        HashMap<String, Integer> tenDaysMap = new HashMap<>();
+
         for (int i = 0; i < want.length; i++) {
-            target.put(want[i], number[i]);
+            wantMap.put(want[i], number[i]);
         }
 
-        Map<String, Integer> map = new HashMap<>();
+        List<String> wantKeySet = new ArrayList<>(wantMap.keySet());
+
         for (int i = 0; i < 10; i++) {
-            map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
-        }
-
-        if (check(target, map)) {
-            answer++;
-        }
-
-        for (int i = 1; i < discount.length- 9; i++) {
-            String previousKey = discount[i - 1];
-            int end = i + 9;
-
-            map.put(previousKey, map.getOrDefault(previousKey, 0) - 1);
-            if (map.get(previousKey) == 0) {
-                map.remove(previousKey);
+            tenDaysMap.put(discount[i], tenDaysMap.getOrDefault(discount[i], 0) + 1);
+            
+            if (check(tenDaysMap, wantMap, wantKeySet)) {
+                answer++;
             }
-            map.put(discount[end], map.getOrDefault(discount[end], 0) + 1);
+        }
 
-            if (check(target, map)) {
+        for (int idx = 10; idx < discount.length; idx++) {
+            tenDaysMap.put(discount[idx], tenDaysMap.getOrDefault(discount[idx], 0) + 1);
+            tenDaysMap.put(discount[idx - 10], tenDaysMap.get(discount[idx - 10]) - 1);
+
+            if (check(tenDaysMap, wantMap, wantKeySet)) {
                 answer++;
             }
         }
@@ -37,12 +33,14 @@ class Solution {
         return answer;
     }
 
-    static boolean check(Map<String, Integer> target, Map<String, Integer> map) {
-        for (String key : map.keySet()) {
-            if (!target.containsKey(key) || map.get(key) != target.get(key)) {
+    private boolean check(HashMap<String, Integer> tenDaysMap, HashMap<String, Integer> wantMap, 
+                          List<String> wantKeySet) {
+        for (String str : wantKeySet) {
+            if (!(tenDaysMap.containsKey(str) && tenDaysMap.get(str).equals(wantMap.get(str)))) {
                 return false;
             }
         }
+
         return true;
     }
 }
