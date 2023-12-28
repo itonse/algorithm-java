afeec7cdd70b1a36c9baad9d50b8601fb47f18d1
Jieun Hong <govl3dnjs@gmail.com>
2023-12-29 00:25:10 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.13 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,55 +1,28 @@
 class Solution {
-    private static String[] wallpaper;
-    
     public int[] solution(String[] wallpaper) {
-        Solution.wallpaper = wallpaper;
         int[] answer = new int[4];
 
-        answer[0] = suchLuy();
-        answer[1] = suchLux();
-        answer[2] = suchRuy() + 1;
-        answer[3] = suchRux() + 1;
+        int left = Integer.MAX_VALUE;
+        int top = Integer.MAX_VALUE;
+        int right = 0;
+        int down = 0;
+
+        for (int row = 0; row < wallpaper.length; row++) {
+            for (int col = 0; col < wallpaper[0].length(); col++) {
+               if (wallpaper[row].charAt(col) == '#') {
+                   left = Math.min(col, left);
+                   right = Math.max(col, right);
+                   top = Math.min(row, top);
+                   down = Math.max(row, down);
+               }
+            }
+        }
+
+        answer[0] = top;
+        answer[1] = left;
+        answer[2] = down + 1;
+        answer[3] = right + 1;
 
         return answer;
-    }
-
-    private int suchLuy() {
-        for (int i = 0; i < wallpaper.length; i++) {
-            if (wallpaper[i].contains("#")) {
-                return i;
-            }
-        }
-        return 0;
-    }
-
-    private int suchRuy() {
-        for (int i = wallpaper.length - 1; i >= 0; i--) {
-            if (wallpaper[i].contains("#")) {
-                return i;
-            }
-        }
-        return 0;
-    }
-
-    private int suchLux() {
-        for (int i = 0; i < wallpaper[0].length(); i++) {
-            for (int j = 0; j < wallpaper.length; j++) {
-                if (wallpaper[j].charAt(i) == '#') {
-                    return i;
-                }
-            }
-        }
-        return 0;
-    }
-
-    private int suchRux() {
-        for (int i = wallpaper[0].length() - 1; i >= 0; i--) {
-            for (int j = 0; j < wallpaper.length; j++) {
-                if (wallpaper[j].charAt(i) == '#') {
-                    return i;
-                }
-            }
-        }
-        return 0;
     }
 }
