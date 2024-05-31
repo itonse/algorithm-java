e4f8e88b7934fd51e610ff393efa50e8c4ab118f
Jieun Hong <govl3dnjs@gmail.com>
2024-05-31 13:44:31 +0900

    [level 1] Title: 공원 산책, Time: 0.17 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -26,8 +26,8 @@ class Solution {
             boolean isBlock = false;
 
             if (direction.startsWith("N")) {
-                if (current[0] - space >= 0 && states[current[0] - space][current[1]] == 1) {
-                    for (int i = current[0]; i > current[0] - space; i--) {
+                if (current[0] - space >= 0) {
+                    for (int i = current[0]; i >= current[0] - space; i--) {
                         if (states[i][current[1]] == 0) {
                             isBlock = true;
                             break;
@@ -40,8 +40,8 @@ class Solution {
                 }
             }
             if (direction.startsWith("S")) {
-                if (current[0] + space < park.length && states[current[0] + space][current[1]] == 1) {
-                    for (int i = current[0]; i < current[0] + space; i++){
+                if (current[0] + space < park.length) {
+                    for (int i = current[0]; i <= current[0] + space; i++){
                         if (states[i][current[1]] == 0) {
                             isBlock = true;
                             break;
@@ -54,8 +54,8 @@ class Solution {
                 }
             }
             if (direction.startsWith("W")) {
-                if (current[1] - space >= 0 && states[current[0]][current[1] - space] == 1) {
-                    for (int i = current[1]; i > current[1] - space; i--) {
+                if (current[1] - space >= 0) {
+                    for (int i = current[1]; i >= current[1] - space; i--) {
                         if (states[current[0]][i] == 0) {
                             isBlock = true;
                             break;
@@ -68,8 +68,8 @@ class Solution {
                 }
             }
             if (direction.startsWith("E")) {
-                if (current[1] + space < park[0].length() && states[current[0]][current[1] + space] == 1) {
-                    for (int i = current[1]; i < current[1] + space; i++) {
+                if (current[1] + space < park[0].length()) {
+                    for (int i = current[1]; i <= current[1] + space; i++) {
                         if (states[current[0]][i] == 0) {
                             isBlock = true;
                             break;
