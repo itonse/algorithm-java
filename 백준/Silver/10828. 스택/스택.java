9ed901c06c1e3e258164bd40d1efc36832d79734
Jieun Hong <govl3dnjs@gmail.com>
2024-10-09 23:40:08 +0900

    [Silver IV] Title: 스택, Time: 176 ms, Memory: 18664 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -4,41 +4,37 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] argr) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int line = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < line; i++) {
             String input = br.readLine();
-            String order = input.split(" ")[0];
 
-            switch(order) {
-                case "push":
-                    stack.push(Integer.parseInt(input.split(" ")[1]));
-                    break;
-                case "top":
-                    if (stack.isEmpty()) {
-                        System.out.println("-1");
-                    } else {
-                        System.out.println(stack.peek());
-                    }
-                    break;
-                case "size":
-                    System.out.println(stack.size());
-                    break;
-                case "empty":
-                    int result = stack.isEmpty() ? 1 : 0;
-                    System.out.println(result);
-                    break;
-                case "pop":
-                    if (stack.isEmpty()) {
-                        System.out.println("-1");
-                    } else {
-                        System.out.println(stack.pop());
-                    }
-                    break;
-                default:
+            if (input.startsWith("push")) {
+                int value = Integer.parseInt(input.substring(5));
+                stack.push(value);
+            } else if (input.equals("top")) {
+                if (stack.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(stack.peek() + "\n");
+                }
+            } else if (input.equals("size")) {
+                bw.write(stack.size() + "\n");
+            } else if (input.equals("empty")) {
+                bw.write((stack.isEmpty() ? 1 : 0) + "\n");
+            } else if (input.equals("pop")) {
+                if (stack.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(stack.pop() + "\n");
+                }
             }
         }
+
+        bw.flush();
+        bw.close();
     }
 }
